3f96f170d73381c5accd36bc54101ca596b0ab16
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-26 15:02:49 -0300

    feat: implements routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,18 @@
+import { Route, Switch } from "react-router-dom";
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routes;
