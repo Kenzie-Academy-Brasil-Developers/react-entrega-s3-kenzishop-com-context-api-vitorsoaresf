9ab725b1523f54ebbfecd6507c420e5b5c8dc6b8
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-28 09:01:45 -0300

    feat: implements services-api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 const Home = () => {
+  const [request, setRequest] = useState([]);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    api
+      .get("/books/v1/volumes?q=harry+potter/")
+      .then((response) => setRequest(response.data.items))
+      .catch((err) => console.log(err));
+  }, []);
+
   return <div>Home</div>;
 };
 
